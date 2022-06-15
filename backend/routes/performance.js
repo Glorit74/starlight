ebb565e123bbc3f2fe3514d6d129107fa5808bfb
Flora <laszlo.t.flora@gmail.com>
2022-06-15 21:31:43 +0200

    get, post new pf

--- backend/routes/performance.js
+++ backend/routes/performance.js
@@ -1,60 +1,71 @@
 const router = require("express").Router();
 const http = require("../utils/http");
+const User = require("../models/user");
 const Performance = require("../models/performance");
-const { auth } = require("../middlewares/auth");
+const auth = require("../middlewares/auth");
 
-router.get(
-  "/performance",
-  /* auth({ block: true }), */ async (req, res) => {
-    const performance = await Performance.find()
-      .limit(perseInt(req.query.limit))
-      .sort({ "venue.date": 1 })
-      .exec();
-    if (!performance) return res.status(404).json([]);
-    res.json({ performance });
-  }
-);
+router.get("/", async (req, res) => {
+  const performance = await Performance.find().limit(req.query.limit);
+  //   .sort({ "venue.date": 1 })
+  //   .exec();
+  if (!performance) return res.status(404).json([]);
+  res.json(performance);
+});
 
-router.post(
-  "/performance",
-  /* auth({ block: true }), */ async (req, res) => {
-    const [
-      title,
-      subTitle,
-      description,
-      author,
-      place,
-      date,
-      time,
-      director,
-      costumeDesign,
-      music,
-      choregrapher,
-      duration,
-      act,
-      picture,
-      video,
-    ] = req.body;
-    const performance = Performance.create({
-      title: title,
-      subTitle: subTitle,
-      description: description,
-      author: author,
-      "venue.place": place,
-      "venue.date": date,
-      "venue.time": time,
-      director: director,
-      costumeDesign: costumeDesign,
-      music: music,
-      choregrapher: choregrapher,
-      duration: duration,
-      act: act,
-      picture: picture,
-      video: video,
-    });
-    if (!performance) return res.status(404).json([]);
-    res.json({ performance });
-  }
-);
+router.post("/", auth({ block: true }), async (req, res) => {
+  const {
+    title,
+    subTitle,
+    description,
+    author,
+    director,
+    costumeDesign,
+    music,
+    choregrapher,
+    duration,
+    act,
+    picture,
+    video,
+  } = req.body;
+  const newPf = new Performance({
+    title: title,
+    subTitle: subTitle,
+    author: author,
+    description: description,
+    director: director,
+    costumeDesign: costumeDesign,
+    music: music,
+    choregrapher: choregrapher,
+    duration: duration,
+    act: act,
+    picture: picture,
+    video: video,
+  });
+  newPf.save();
+  if (!newPf) return res.status(404).json([]);
+  res.json(newPf);
+});
+
+router.post("/venue", auth({ block: true }), async (req, res) => {
+  const { title, place, date, time } = req.body;
+  const venue = { place: place, date: date, time: time };
+  let newPf = await Performance.findOneAndUpdate(
+    { title: title },
+    { $push: { venue: venue } },
+    { upsert: true, new: true }
+  );
+  res.status(200).json(newPf);
+});
+
+router.post("/actor", auth({ block: true }), async (req, res) => {
+  const { title, name, role } = req.body;
+  const actor = { name: name, role: role };
+  let newPf = await Performance.findOneAndUpdate(
+    { title: title },
+    { $push: { actor: actor } },
+    { upsert: true, new: true }
+  );
+  res.status(200).json(newPf);
+});
 
 module.exports = router;
