8db7a38a8aa69b29c0d1039d9b7572a1f0d3fdca
Flora <laszlo.t.flora@gmail.com>
2022-07-04 08:38:30 +0200

    FE actors

--- frontend/src/pages/Actor.js
+++ frontend/src/pages/Actor.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { toDoApi } from "../api/toDoApi";
+import Navbar from "../components/Navbar";
 
 function Actor() {
-  return <div>Actor</div>;
+  const { get } = toDoApi();
+  //   const [actors, setActors] = useState({
+  //     name: "",
+  //     description: "",
+  //     picture: "",
+  //     awards: [],
+  //   });
+  const [actors, setActors] = useState([]);
+
+  const getActors = async () => {
+    const responsePf = await get("/actor");
+    setActors(responsePf.data);
+  };
+
+  useEffect(() => {
+    getActors();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      <header className="actor_header">Színművészeink</header>
+      <button onClick={(e) => console.log(actors)}>GET ACTOR</button>
+      {actors.map((actor) => (
+        <figure className="actor_figure">
+          <img
+            src={actor.picture}
+            alt={actor.name}
+            width="100wh"
+            className="actor_img"
+          ></img>
+          <figcaption className="actor_caption">{actor.name}</figcaption>
+        </figure>
+      ))}
+    </div>
+  );
 }
 
 export default Actor;
